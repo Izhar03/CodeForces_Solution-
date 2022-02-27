255dcd16bb31a7f1396c53612ca7d9f0ea936a75
IZHAR AHMAD <55507957+Izhar03@users.noreply.github.com>
2022-02-27 19:46:48 +0530

    Added 263A Beautiful Matrix on 26 FEB 22- NOT DONE

---
+++ 263A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+  int x,y;
+
+  for( int i=1;i<5;i++){
+    for(int j=1;j<6;j++){
+      cin>>x;
+    }
+    cin>>y;
+
+  }
+
+
+
+}
